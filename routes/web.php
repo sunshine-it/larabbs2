8c4068cc687ba5177692d2e29b015472c190bb3b
Allen <pazmnew@163.com>
2018-08-16 12:45:57 +0000

    User can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 // 分类下的话题列表
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+// 上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
