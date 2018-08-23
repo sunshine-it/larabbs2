d09b304b1a5338ec3d57b2e10a6764265a1159b1
Allen <pazmnew@163.com>
2018-08-23 07:48:36 +0000

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 // 首页
 Route::get('/', 'PagesController@root')->name('root');
 // 用户认证功能 | 用户认证路由
@@ -17,7 +7,8 @@ Auth::routes();
 //用户资源
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 // 话题列表
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index','create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 // 分类下的话题列表
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 // 上传图片
