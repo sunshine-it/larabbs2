04639722c6e70f8576d056a8f151be16cb278b6e
Allen <pazmnew@163.com>
2018-10-11 12:55:13 +0000

    Add manage contents permission and visit horizon dashboard permission

--- app/Policies/Policy.php
+++ app/Policies/Policy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 
+// 策略类
 class Policy
 {
     use HandlesAuthorization;
@@ -15,8 +16,9 @@ class Policy
 
     public function before($user, $ability)
 	{
-	    // if ($user->isSuperAdmin()) {
-	    // 		return true;
-	    // }
+        // 如果用户拥有管理内容的权限的话，即授权通过
+	    if ($user->can('manage_contents')) {
+	    		return true;
+	    }
 	}
 }
