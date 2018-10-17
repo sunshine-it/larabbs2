1b7986c1a7dae287d2e73922cf244a5bac544f79
Allen <pazmnew@163.com>
2018-10-17 12:15:53 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -7,8 +7,17 @@ use App\Models\User;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
+// 用户模型监控器
 class UserObserver
 {
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
+
     public function creating(User $user)
     {
         //
