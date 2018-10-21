b41729e8b89e5fbfd0ebcb966c068ca9ed2852a3
Allen <pazmnew@163.com>
2018-10-21 16:56:53 +0000

    user can register using phone

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\Api\UserRequest;
+
+// 用户控制器类
+class UsersController extends Controller
+{
+    public function store(UserRequest $request)
+    {
+        $verifyData = \Cache::get($request->verification_key);
+
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+        // 比对验证码是否与缓存中一致
+        if (!hash_equals($verifyData['code'], $request->verification_code)) {
+            // 返回401
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+        // 创建用户
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
+        ]);
+        // 清除验证码缓存
+        \Cache::forget($request->verification_key);
+        return $this->response->created();
+    }
+}
---
+++ app/Http/Requests/Api/UserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Dingo\Api\Http\FormRequest;
+
+// 用户表单验证类
+class UserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'password' => 'required|string|min:6',
+            'verification_key' => 'required|string',
+            'verification_code' => 'required|string',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'verification_key' => '短信验证码 key',
+            'verification_code' => '短信验证码',
+        ];
+    }
+
+}
