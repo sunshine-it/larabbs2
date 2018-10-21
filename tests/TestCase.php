b859ae18f7d462d4ad46e211d22e6b42202aaf14
Allen <pazmnew@163.com>
2018-10-21 02:58:21 +0000

    add easysms

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function sms()
+    {
+        $sms = app('easysms');
+        try {
+            $sms->send(13242712858, [
+                'content'  => '【云盛网络测试】您的验证码是6666。如非本人操作，请忽略本短信',
+            ]);
+        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+            $message = $exception->getException('yunpian')->getMessage();
+            dd($message);
+        }
+       }
 }
