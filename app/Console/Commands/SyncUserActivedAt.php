70801bc8cfdd72236c7a65dbb62d6f6a6c97e39b
Allen <pazmnew@163.com>
2018-10-26 16:02:55 +0000

    add captcha

--- app/Console/Commands/SyncUserActivedAt.php
+++ app/Console/Commands/SyncUserActivedAt.php
@@ -11,10 +11,8 @@ class SyncUserActivedAt extends Command
 
     protected $description = '将用户最后登录时间从 Redis 同步到数据库中';
 
-
     public function handle(User $user)
     {
         $user->syncUserActivedAt();
-        $this->info("同步成功！");
     }
 }
--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -10,26 +10,43 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
+
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
+
         // 除了正式环境外，其他环境，默认不真实发送短信,短信验证码默认为1234
         if (!app()->environment('production')) {
             $code = '1234';
         }else{
             // 生成4位随机数，左侧补0
             $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
             try {
                 $result = $easySms->send($phone, [
                     'content' => "【云盛网络测试】您的验证码是{$code}。如非本人操作，请忽略本短信"
                 ]);
-            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-                $message = $exception->getException('yunpian')->getMessage();
-                return $this->response->errorInternal($message ?? '短信发送异常');
+            } catch (\GuzzleHttp\Exception\ClientException $exception) {
+                $response = $exception->getResponse();
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
             }
         }
         $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
